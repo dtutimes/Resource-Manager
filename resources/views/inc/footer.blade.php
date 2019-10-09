fdacf82f977629b970be27aa81a736d66696db6b
nandana <nandana.tiwari.99@gmail.com>
2019-10-10 01:17:17 +0400

    removing facebook widget

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -22,11 +22,29 @@
                         <div class="footer-col-inner" style="text-align:center;">
                             <h3 style="color:white;">Connect with us</h3>
 
+                            <div class="widget subscribe-widget clearfix dark" style="margin:30px 0; width:300px; display:inline-block;">
+                                <h5 style="text-align:center;"><strong>Subscribe</strong> to Our Newsletter:</h5>
+                                <div class="widget-subscribe-form-result" style="display:inline-block;"></div>
+
+                                <form id="widget-subscribe-form" action="{{ route('subscribers.join') }}" role="form" method="post" class="nobottommargin">
+                                    <div class="input-group divcenter" style="margin-top:-20px;">
+                                        @csrf
+                                        <span class="input-group-addon"><i class="icon-email2"></i></span>
+                                        <input type="email" id="widget-subscribe-form-email" name="email" class="form-control required email" placeholder="Enter your Email">
+                                        <span class="input-group-btn">
+                                                        <button class="btn btn-success" type="submit" style="background-color:#1ABC9C;">Subscribe</button>
+                                                    </span>
+                                    </div>
+                                </form>
+                            </div>
+
+                            <!--
                             <div class="footer-col col-md-12 col-mf-offset-1 col-sm-12 col-xs-12 col-xs-offset-0" style=" box-sizing:border-box;">
                                 <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fdtutimes&tabs&width=270&height=70&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
                                     width="270" height="130" style="border:none;overflow:auto;margin:20px 0px;display:inline-block;"
                                     scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
                             </div>
+                          -->
                         </div>
                         <!--//footer-col-inner-->
 
@@ -46,6 +64,7 @@
         <!--//footer-content-->
 
         <div class="bottom-bar" style="text-align:center;">
+          <!--
             <div class="widget subscribe-widget clearfix dark" style="margin:30px 0; width:300px; display:inline-block;">
                 <h5 style="text-align:center;"><strong>Subscribe</strong> to Our Newsletter:</h5>
                 <div class="widget-subscribe-form-result" style="display:inline-block;"></div>
@@ -61,9 +80,10 @@
                     </div>
                 </form>
             </div>
+          -->
 
             <div class="container" style="margin-top:30px">
-                <small class="copyright col-md-12 col-sm-12 col-xs-12" style="color:white; text-align:center;">Copyright &copy; 2018 DTU Times</small>
+                <small class="copyright col-md-12 col-sm-12 col-xs-12" style="color:white; text-align:center;">Copyright &copy; 2019 DTU Times</small>
             </div>
             <!--//container-->
         </div>
